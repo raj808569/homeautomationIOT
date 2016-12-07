683a2ce678d547aacb71c644261963535760abc4
shubham raj <shubhamraj97@gmail.com>
2016-12-07 07:23:35 +0000

    Update index.js

--- index.js
+++ index.js
@@ -8,46 +8,71 @@ app.set('port',(process.env.PORT||3000));
 http.listen(app.get('port'),function(){
   console.log("listening to port number"+app.get('port'));
 });
-var Gpio = require('onoff').Gpio;
-led=new Gpio(4,'out');
+var off1,off2;
+var pyshell=require('python-shell');
 app.get('/',function(req,res){
   res.sendFile(__dirname + '/index.html');
 });
 io.sockets.on('connection', function (socket) {
 
 //light1onoff code
-				socket.on('light1off',function(data){console.log('off');
-						{io.sockets.in(socket.id).emit('light1offdone',0);
-                     		}
-					});
-socket.on('light1',function(data){ console.log('working');
-                                   
-                                   if(data==1)
-				{io.sockets.in(socket.id).emit('light1done',1);
-                     		}
 				
+socket.on('light1',function(data){ console.log('working');
+                                  
+                                
+				pyshell.run('a.py',function(err){
+						if(err) throw err;
+					});
 				
 
                                 });
 				
+socket.on('light1off',function(data){console.log('off');
+				
+				pyshell.run('b.py',function(err){
+						if(err) throw err;
+					});
+					
+					});
 
+//fan1onoff code
+
+socket.on('fan1',function(data){ console.log('working');
+                 
+                               pyshell.run('e.py',function(err){
+						if(err) throw err;
+					});
+				
+				
+
+                                });
+
+   socket.on('fan1off',function(data){console.log('off');
+			pyshell.run('f.py',function(err){
+						if(err) throw err;
+					});
+									
+                     		
+					});
 
 //light2onoff code
 
 socket.on('light2',function(data){ console.log('working');
-                                   var Gpio = require('onoff').Gpio,
-                                   led=new Gpio(3,'out');
-                                   if(data==1)
-				{io.sockets.in(socket.id).emit('light2done',1);
-                     		}
-				
-				socket.on('light2off',function(data){console.log('off');
-						{io.sockets.in(socket.id).emit('light2offdone',0);
-                     		}
+                 
+                               pyshell.run('c.py',function(err){
+						if(err) throw err;
 					});
+				
+				
 
                                 });
 
-   
+   socket.on('light2off',function(data){console.log('off');
+			pyshell.run('d.py',function(err){
+						if(err) throw err;
+					});
+									
+                     		
+					});
                                               });
 
